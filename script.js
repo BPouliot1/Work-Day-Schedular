8cad801a871229b39f04025b29d844de299f959b
Brandon Pouliot <brandonpouliot@gmail.com>
2024-04-28 15:56:17 -0400

    added setInterval to update the colors every minute

--- script.js
+++ script.js
@@ -18,6 +18,8 @@ $(function () {
     });
   }
   updateColors();
+
+  setInterval(updateColors, 60000);
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
